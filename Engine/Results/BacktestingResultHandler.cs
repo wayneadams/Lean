d3a9e02846831b27b8ffb7486a6b6550a6bb9342
Gerardo Salazar <gsalaz9800@gmail.com>
2020-01-14 14:46:35 -0800

    Address Martin's review - code refactoring and cleaning
    * Removed Sample[a-zA-Z]+ methods from IResultHandler definition
    * Converted Sample[a-zA-Z]+ methods from public to protected
    * Updated inheritors of BaseResultHandler to use new accessibility
    modifiers
    * Removes useless code in ResolutionSwitchingAlgorithm

--- Engine/Results/BacktestingResultHandler.cs
+++ Engine/Results/BacktestingResultHandler.cs
@@ -564,7 +564,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="time">Time for the sample</param>
         /// <param name="unit">Unit of the sample</param>
         /// <param name="value">Value for the chart sample.</param>
-        public void Sample(string chartName, string seriesName, int seriesIndex, SeriesType seriesType, DateTime time, decimal value, string unit = "$")
+        protected void Sample(string chartName, string seriesName, int seriesIndex, SeriesType seriesType, DateTime time, decimal value, string unit = "$")
         {
             // Sampling during warming up period skews statistics
             if (Algorithm.IsWarmingUp)
@@ -603,7 +603,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// </summary>
         /// <param name="time">Current backtest time.</param>
         /// <param name="value">Current equity value.</param>
-        public void SampleEquity(DateTime time, decimal value)
+        protected void SampleEquity(DateTime time, decimal value)
         {
             //Sample the Equity Value:
             Sample("Strategy Equity", "Equity", 0, SeriesType.Candle, time, value);
@@ -617,7 +617,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// </summary>
         /// <param name="time">Current backtest date.</param>
         /// <param name="value">Current daily performance value.</param>
-        public virtual void SamplePerformance(DateTime time, decimal value)
+        protected virtual void SamplePerformance(DateTime time, decimal value)
         {
             //Added a second chart to equity plot - daily perforamnce:
             Sample("Strategy Equity", "Daily Performance", 1, SeriesType.Bar, time, value, "%");
@@ -629,7 +629,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// <param name="time">Current backtest date.</param>
         /// <param name="value">Current benchmark value.</param>
         /// <seealso cref="IResultHandler.Sample"/>
-        public void SampleBenchmark(DateTime time, decimal value)
+        protected void SampleBenchmark(DateTime time, decimal value)
         {
             Sample("Benchmark", "Benchmark", 0, SeriesType.Line, time, value);
         }
@@ -638,7 +638,7 @@ namespace QuantConnect.Lean.Engine.Results
         /// Add a range of samples from the users algorithms to the end of our current list.
         /// </summary>
         /// <param name="updates">Chart updates since the last request.</param>
-        public void SampleRange(List<Chart> updates)
+        protected void SampleRange(List<Chart> updates)
         {
             lock (ChartLock)
             {
@@ -733,17 +733,6 @@ namespace QuantConnect.Lean.Engine.Results
         {
             var statusPacket = new AlgorithmStatusPacket(_algorithmId, _projectId, status, message);
             MessagingHandler.Send(statusPacket);
-        }
-
-        /// <summary>
-        /// Sample the asset prices to generate plots.
-        /// </summary>
-        /// <param name="symbol">Symbol we're sampling.</param>
-        /// <param name="time">Time of sample</param>
-        /// <param name="value">Value of the asset price</param>
-        public void SampleAssetPrices(Symbol symbol, DateTime time, decimal value)
-        {
-            //NOP. Don't sample asset prices in console.
         }
 
         /// <summary>
